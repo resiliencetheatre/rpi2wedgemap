959f41bd43b05b93c0a463b881b22a159d6b976a
info <info@resilience-theatre.com>
2024-04-02 04:16:15 +0300

    * Corrected fstab mount timeout value. * Modified partitioning service to create second partition on first boot. * Partitioning script outputs to eInk it's status. * Added parted to image.

--- board/raspberrypi/fs_edgemap/usr/bin/create-partition.sh
+++ board/raspberrypi/fs_edgemap/usr/bin/create-partition.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 if [ -d "/mnt/lost+found" ] 
 then
-    echo "/mnt partition found!" 
+    echo "/mnt partition found!"
+    /bin/edgemap-ui -m "/mnt partition found" -d 2000
 else
+    /bin/edgemap-ui -m "Creating partition for maps"
     echo "Creating third partition"
     TARGET_DEV=/dev/mmcblk0
     parted --script $TARGET_DEV 'mkpart primary ext4 1100 -1'
     mkfs.ext4 -F -F ${TARGET_DEV}p2
+    /bin/edgemap-ui -m "Partition and ext4 FS created!"
 fi
 exit 0
 
